c8f7ed272d3288a284534b9d1c3dfa4b4d23bc8a
Arya1315 <aryaingole535@email.com>
2026-06-04 14:40:55 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// Project-level build.gradle
+// -------------------------------------------------------
+// Top-level build file. Add configuration options common
+// to all sub-projects/modules here.
+// -------------------------------------------------------
+
+plugins {
+    id("com.android.application") version "8.4.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false // ADD THIS LINE
+
+}
